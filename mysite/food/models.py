8f3a9f87f28973091200107bca5adbef1c96ee80
ShokhrukhG93 <shokhrukhgaybullaev.93@gmail.com>
2024-02-04 14:03:34 +0500

    stacfiles

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -9,3 +9,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://static.wixstatic.com/media/bf242e_6133b4ae6a104cc2b50d70179f35efea~mv2.jpg/v1/fill/w_500,h_376,al_c,lg_1,q_80,enc_auto/food-placeholder.jpg")
