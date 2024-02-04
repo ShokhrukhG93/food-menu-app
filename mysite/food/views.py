00dba7f14bcb5f69c5f748fd328a8337962aab63
ShokhrukhG93 <shokhrukhgaybullaev.93@gmail.com>
2024-02-04 11:00:55 +0500

    templates

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -2,12 +2,26 @@ from .models import Item
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    # template = loader.get_template('food/index.html')
+    context = {
+        'item_list': item_list,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'food/index.html', context)
 
 
 def item(request):
     return HttpResponse('This is an item view')
+
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item,
+    }
+    return render(request, 'food/detail.html', context)
